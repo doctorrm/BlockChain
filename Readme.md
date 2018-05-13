fd2c703723e5a2951fd39f266fdb6bc79dd6864f
doctorrm <13710638629@163.com>
2018-05-13 19:59:59 +0800

    block chain with check valid and simple mining.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+On the bitcoin network nodes share their blockchains and the longest valid chain is accepted by the network. What’s to stop someone tampering with data in an old block then creating a whole new longer blockchain and presenting that to the network ?** Proof of work**. 
+
+The hashcash proof of work system means it takes considerable time and computational power to create new blocks. Hence the attacker would need more computational power than the rest of the peers combined.
--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -6,17 +6,33 @@ public class Block {
 	public String previousHash;
 	private String data; // our data will be a simple message.
 	private long timeStamp; // as number of milliseconds since 1/1/1970.
+	private int nonce;
 
 	// Block Constructor.
 	public Block(String data, String previousHash) {
 		this.data = data;
 		this.previousHash = previousHash;
 		this.timeStamp = new Date().getTime();
+
 		this.hash = calculateHash(); // Making sure we do this after we set the other values.
 	}
 
+	// Calculate new hash based on blocks contents
 	public String calculateHash() {
-		String calculatedhash = StringUtil.applySha256(previousHash + Long.toString(timeStamp) + data);
+		String calculatedhash = StringUtil
+				.applySha256(previousHash + Long.toString(timeStamp) + Integer.toString(nonce) + data);
 		return calculatedhash;
+	}
+
+	/**
+	 * 挖矿：整出（一直尝试计算）头部前difficult（5）位都是0的hash。很大程度依赖于时间戳
+	 */
+	public void mineBlock(int difficulty) {
+		String target = new String(new char[difficulty]).replace('\0', '0'); // Create a string with difficulty * "0"
+		while (!hash.substring(0, difficulty).equals(target)) {
+			nonce++;
+			hash = calculateHash();
+		}
+		System.out.println("Block Mined!!! : " + hash);
 	}
 }
